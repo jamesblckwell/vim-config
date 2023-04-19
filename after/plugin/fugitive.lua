84406c2c4134e0b9bca14810c587d26c3602424a
James Blackwell <james.blackwell@havashelia.com>
2023-04-19 17:14:38 +0100

    More commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,6 +1,6 @@
 -- fugitive.lua
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-vim.keymap.set("n", "<leader>gp", vim.cmd.Git push);
+vim.keymap.set("n", "<leader>gp", ':Git push<CR>');
 vim.keymap.set("n", "<leader>gca", ':Git commit -am ""<LEFT>');
-vim.keymap.set("n", "<leader>gcf", ':Git commit -m ""<LEFT>');
+vim.keymap.set("n", "<leader>gcf", ':Git commit % -m ""<LEFT>');
